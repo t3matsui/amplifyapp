44bb951c29551ca3351afb94739b5c322cfdf377
Taro Matsui <t_matsui@outlook.com>
2022-01-10 19:18:05 +0900

    modify test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
+/* eslint-disable testing-library/no-node-access */
+/* eslint-disable testing-library/no-container */
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const {container} = render(<App />);
+  expect(container.querySelector('amplify-authenticator')).toBeDefined();
 });
